44f5505275fb839aa70ec944f66e1bc488f41be4
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-04 20:20:09 -0500

    rs added

---
+++ rust/data-types/target/debug/deps/data_types-1e901c7fa277a940.d
@@ -0,0 +1,5 @@
+/home/edith/Desktop/code/practice/code-practice-main/rust/data-types/target/debug/deps/data_types-1e901c7fa277a940.rmeta: src/main.rs
+
+/home/edith/Desktop/code/practice/code-practice-main/rust/data-types/target/debug/deps/data_types-1e901c7fa277a940.d: src/main.rs
+
+src/main.rs:
---
+++ rust/data-types/target/debug/deps/data_types-afb9366bd21173a1.d
@@ -0,0 +1,5 @@
+/home/edith/Desktop/code/practice/code-practice-main/rust/data-types/target/debug/deps/data_types-afb9366bd21173a1.rmeta: src/main.rs
+
+/home/edith/Desktop/code/practice/code-practice-main/rust/data-types/target/debug/deps/data_types-afb9366bd21173a1.d: src/main.rs
+
+src/main.rs:
