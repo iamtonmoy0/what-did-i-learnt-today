1e3e03fa39a3074176c0e82630076652034e92b7
iamtonmoy0 <tonmoy2code@gmail.com>
2023-04-11 08:11:52 -0400

    cart

--- java script/advance obj/cart/cart.js
+++ java script/advance obj/cart/cart.js
@@ -6,12 +6,38 @@ const addProduct=()=>{
 	productName.value='';
 	productQun.value='';
 	// console.log(product,quantity)
-	displayProduct(product,quantity)
+	displayProduct(product,quantity);
 }
 const displayProduct=(product,quantity)=>{
 	const ul=document.getElementById('productContainer')
 	const li= document.createElement('li');
 	li.innerText=`${product}:${quantity}`;
 	ul.appendChild(li)
+saveProduct(product,quantity)
 
 }
+// get stored shopping cart
+const getStored=()=>{
+	let cart={};
+	const storedCart=localStorage.getItem('cart')
+	if(storedCart){
+		cart=JSON.parse(storedCart)
+	}return cart;
+}
+const saveProduct=(product,quantity)=>{
+	const cart =getStored();
+	cart[product]=quantity
+	const str=JSON.stringify(cart)
+	localStorage.setItem('cart',str)
+}
+const localData=()=>{
+const savedData=	getStored();
+for(const product in savedData){
+	const quantity=savedData[product]
+console.log(product,quantity)
+displayProduct(product,quantity)
+}
+	
+
+}
+localData()
