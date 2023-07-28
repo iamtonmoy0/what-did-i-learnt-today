632848d005eadab270716e3728d7d6de858b9bea
iamtonmoy0 <tonmoy2code@gmail.com>
2023-07-28 09:33:39 -0400

    req added

--- nodejs/expressJsLWS/appObj/index.js
+++ nodejs/expressJsLWS/appObj/index.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const cookieParser=require('cookie-parser') 
 const app = express()
+app.use(express.json());
+app.use(cookieParser())
 
+const adminRoute=express.Router();
+adminRoute.get('/admin',(req,res)=>{
+  console.log(req.cookies)
+  res.send('this is admin route')
+})
+app.use('/dashboard',adminRoute)
 app.get('/', (req, res) => {
+  console.log(req)
   res.send('hello world')
 })
 
