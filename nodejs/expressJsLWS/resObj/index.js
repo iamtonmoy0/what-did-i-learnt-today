df4982e83dde6fba6252a29a32457647319c6f2f
iamtonmoy0 <tonmoy2code@gmail.com>
2023-07-29 11:01:14 -0400

    node js

---
+++ nodejs/expressJsLWS/resObj/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const myMiddleware=(req,res,next)=>{
+	console.log('loading')
+	next()
+}
+app.use(myMiddleware)
+// app.use(myMiddleware)
+app.get('/',(req,res)=>{
+	res.send('hello world')
+})
+
+app.listen(3000 ,console.log('server is running on port 3000'));
