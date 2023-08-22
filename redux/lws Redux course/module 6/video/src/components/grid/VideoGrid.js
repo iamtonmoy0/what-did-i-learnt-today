ccc0bc642cf12c35f66f89278945ec75d65531da
iamtonmoy0 <tonmoy2code@gmail.com>
2023-08-22 12:20:31 -0400

    module 6

--- redux/lws Redux course/module 6/video/src/components/grid/VideoGrid.js
+++ redux/lws Redux course/module 6/video/src/components/grid/VideoGrid.js
@@ -1,13 +1,25 @@
+import { useEffect } from "react";
 import VideoGridItem from "./VideoGridItem";
-
+import {useDispatch, useSelector} from 'react-redux'
+import { fetchVideos } from "../../features/videos/videosSlice";
+import Loading from "../ui/Loading";
 export default function VideGrid() {
-    return (
+    const dispatch = useDispatch();
+    const{videos,isLoading,isError,error}=useSelector(state=>state.videos);
+
+    useEffect(()=>{
+        dispatch(fetchVideos())
+    },[dispatch])
+    // statement
+    let content;
+    if(isLoading) content = <Loading/>;
+    if(isError && !isLoading) content=  <div className="col-span-12">{error}</div>
+    if(!isError && !isLoading && videos?.length>0) content=videos.map(video=><VideoGridItem key={video.id} video={video}/>) 
+        return (
         <section className="pt-12">
             <section className="pt-12">
                 <div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
-                    <VideoGridItem />
-
-                    {/* <div className="col-span-12">some error happened</div> */}
+                   {content}
                 </div>
             </section>
         </section>
