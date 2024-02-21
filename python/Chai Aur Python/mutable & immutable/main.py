f60a450c25eb2899e0bd8bfbc38514c187ff52e8
iamtonmoy0 <tonmoy2code@gmail.com>
2024-02-21 23:03:00 +0600

    python

--- python/Chai Aur Python/mutable & immutable/main.py
+++ python/Chai Aur Python/mutable & immutable/main.py
@@ -1,1 +1,12 @@
 # mutable and immutables in python
+a=[12,12,35]
+b=a
+a[0]=15
+print(a,"this is a")
+print(b,"this is b")
+
+import copy
+h=[1,23,2]
+# h2=copy.copy(h)
+h2=copy.deepcopy(h)
+print(h2)
