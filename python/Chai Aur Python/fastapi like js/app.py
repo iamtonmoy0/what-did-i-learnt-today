66b7850c9d9d242bc4f393d8d914e45748dfce86
iamtonmoy0 <tonmoy2code@gmail.com>
2024-02-23 19:57:29 +0600

    py

--- python/Chai Aur Python/fastapi like js/app.py
+++ python/Chai Aur Python/fastapi like js/app.py
@@ -7,6 +7,9 @@ app=FastAPI()
 @app.get("/")
 def home():
     return {"data":"this is home"}
+@app.get("/hello")
+def hello():
+    return {"data":"kjgjkfgjhgjhgjg"}
 
 import uvicorn
 uvicorn.run(app)
