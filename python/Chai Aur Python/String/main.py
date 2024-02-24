21b019a2eafb17591a1e464ce1b602d423887bcc
iamtonmoy0 <tonmoy2code@gmail.com>
2024-02-24 15:11:07 +0600

    python

---
+++ python/Chai Aur Python/String/main.py
@@ -0,0 +1,20 @@
+# slice
+a="hello world"
+b=a[0:6]
+# print(b)
+# hopping 
+# print(a[0:5:2])
+
+# strip
+# print(a.strip())
+
+# upper and lower	
+# print("Hello World".upper(), "Hello World".lower())
+
+texxt="hello wordl an bd python is fun"
+# print(texxt.find("fun"))
+
+# list to string
+fruit=["apple","banana","orange"]
+print(", ".join(fruit))
+# output  apple, banana, orange
