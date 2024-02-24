e487b6ee24430cca1717f9d29219173936ab3c02
iamtonmoy0 <tonmoy2code@gmail.com>
2024-02-24 12:15:55 +0600

    python

--- python/Chai Aur Python/Numbers/main.py
+++ python/Chai Aur Python/Numbers/main.py
@@ -1,3 +1,24 @@
 a=1
 b=3
-print(a+b)
+# print(a+b)
+# math package
+import math
+# print(math.floor(3.9))
+# print(math.floor(-3.3))
+# random
+import random
+
+ch=["john","jack","jackob"]
+# print(random.choice(ch))
+# decimal
+from decimal import Decimal
+
+# print(Decimal("0.1")+Decimal("0.1")+Decimal("0.1"))
+
+# Fractions
+import fractions
+
+
+
+# set type
+
