90576ab56383cdc822136fcdd0237d0a5ce7ca62
iamtonmoy0 <tonmoy2code@gmail.com>
2024-02-27 01:57:27 +0600

    python

---
+++ python/Chai Aur Python/Dictonary/main.py
@@ -0,0 +1,6 @@
+test={"hello":"world","chai":"code","i am ":"coder"}
+# print(test["hello"])
+ 
+ 
+for key,value in test.items():
+    print(value,key)
---
+++ python/Chai Aur Python/List/main.py
@@ -0,0 +1,8 @@
+# list is also called array  in some other programming languages
+name=["john","jack","doe","sophie"]
+# print(name)
+for n in name:
+    print(n,end="-")
+
+if "john" in name:
+    print("i have")
