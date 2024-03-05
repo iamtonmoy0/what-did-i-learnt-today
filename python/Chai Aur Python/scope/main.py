542cbe34a3fb9aa8d05eedac24fce07efc06bc4c
iamtonmoy0 <tonmoy2code@gmail.com>
2024-03-05 20:55:28 +0600

    python

--- python/Chai Aur Python/scope/main.py
+++ python/Chai Aur Python/scope/main.py
@@ -1,2 +1,14 @@
 def hello():
     print("hello world")
+    
+def function():
+    # this is global
+    global x
+    x=12
+
+
+# closure
+def closure(num):
+    def actual(x):
+        return x ** num
+    return actual
