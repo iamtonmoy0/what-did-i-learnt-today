c53cff97f9c3473197fc876fe5c267c6a0317589
iamtonmoy0 <tonmoy2code@gmail.com>
2024-04-05 00:35:21 +0600

    redux

---
+++ redux/lws Redux course/module 6/video-app/src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import videoReducer from "../features/videos/videoSlice";
+
+export const store = configureStore({
+  reducer: {
+    videos: videoReducer,
+  },
+});
