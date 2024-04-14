2a9eaaeee23316fe647bf0ce79422931c0c4cbd0
iamtonmoy0 <tonmoy2code@gmail.com>
2024-04-15 04:30:52 +0600

    rtk

--- redux/lws Redux course/module 8/rtk/src/components/pages/Video.tsx
+++ redux/lws Redux course/module 8/rtk/src/components/pages/Video.tsx
@@ -1,20 +1,37 @@
+import { useParams } from "react-router-dom";
+import { useGetVideoQuery } from "../../features/api/apiSlice";
 import Description from "../video/Description";
 import Player from "../video/Player";
 import RelatedVideos from "../video/related/RelatedVideos";
 
 export default function Video() {
-    return (
-        <section className="pt-6 pb-20 min-h-[calc(100vh_-_157px)]">
-            <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
-                <div className="grid grid-cols-3 gap-2 lg:gap-8">
-                    <div className="col-span-full w-full space-y-8 lg:col-span-2">
-                        <Player />
-                        <Description />
-                    </div>
-
-                    <RelatedVideos />
-                </div>
-            </div>
-        </section>
+  const { id } = useParams();
+  const { data: video, isLoading, isError } = useGetVideoQuery(id);
+  let content = null;
+  if (isLoading) {
+    content = <div>loading</div>;
+  }
+  if (!isLoading && isError) {
+    content = <div>error</div>;
+  }
+  if (!isLoading && !isError && video) {
+    content = (
+      <>
+        <Player link={video.link} />
+        <Description video={video} />
+      </>
     );
+  }
+  return (
+    <section className="pt-6 pb-20 min-h-[calc(100vh_-_157px)]">
+      <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
+        <div className="grid grid-cols-3 gap-2 lg:gap-8">
+          <div className="col-span-full w-full space-y-8 lg:col-span-2">
+            {content}
+          </div>
+          <RelatedVideos />
+        </div>
+      </div>
+    </section>
+  );
 }
--- redux/lws Redux course/module 8/rtk/src/routes/router.tsx
+++ redux/lws Redux course/module 8/rtk/src/routes/router.tsx
@@ -1,8 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../components/pages/Home";
+import Video from "../components/pages/Video";
+
 const router = createBrowserRouter([
-  { path: "/", element: <App />, children: [{ path: "/", element: <Home /> }] },
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/videos/:id", element: <Video /> },
+    ],
+  },
 ]);
 
 export default router;
