3f6fae78fdbf299b3cd1a6f7e5215fddd312cc84
iamtonmoy0 <tonmoy2code@gmail.com>
2024-04-17 06:46:32 +0600

    rtk

--- redux/lws Redux course/module 8/rtk/src/features/api/apiSlice.ts
+++ redux/lws Redux course/module 8/rtk/src/features/api/apiSlice.ts
@@ -5,23 +5,41 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:9000",
   }),
+  tagTypes: ["Videos", "Video"],
   endpoints: (builder) => ({
     getVideos: builder.query({
       query: () => "/videos",
+      keepUnusedDataFor: 5,
+      providesTags: ["Videos"],
     }),
     getVideo: builder.query({
       query: (id) => `/videos/${id}`,
+      providesTags: (result, error, arg) => [{ type: "video", id: arg }],
     }),
     getRelatedVideos: builder.query({
-      query: (title) => {
+      query: ({ id, title }) => {
         const tags = title.split(" ");
         const like = tags.map((tag) => `title_like=${tag}`);
         const queryString = `/videos?${like.join("&")}&_limit=4`;
         return queryString;
       },
+      providesTags: (result, error, arg) => [
+        { type: "RelatedVideo", id: arg.id },
+      ],
+    }),
+    addVideo: builder.mutation({
+      query: (data) => {
+        url: "/videos";
+        method: "POST";
+        body: data;
+      },
     }),
   }),
 });
 
-export const { useGetVideosQuery, useGetVideoQuery, useGetRelatedVideosQuery } =
-  apiSlice;
+export const {
+  useGetVideosQuery,
+  useGetVideoQuery,
+  useGetRelatedVideosQuery,
+  useAddVideoMutation,
+} = apiSlice;
