4c7e451ef0aaeb80856120b900947ffd3fbc2257
iamtonmoy0 <tonmoy2code@gmail.com>
2025-01-25 20:54:26 +0600

    `Removed main.py files from days 1, 2, and 3 of the 100-days-of-code project.`

--- python/100-days-of-code/day 1/main.py
+++
@@ -1,6 +0,0 @@
-print("hello welcome to generator")
-
-city= input("enter the city name\n")
-pet= input("your pet name\n")
-
-print ("your brand name could be" + " " + city + " " + pet)
--- python/100-days-of-code/day 2/main.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the calculator")
-bill = float(input ("wat was the total bill? $"))
-tip = int(input("how much you want to tip"))
-person = int(input("how many people with you are"))
-bill_with_tip= tip/100*bill +bill
-
-
-print(bill_with_tip)
