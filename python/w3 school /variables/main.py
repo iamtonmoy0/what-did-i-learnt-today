45d1103b75102e3629c6795a4ae0902a5f390988
iamtonmoy0 <tonmoy2code@gmail.com>
2025-01-25 16:49:40 +0600

    variables

---
+++ python/w3 school /variables/main.py
@@ -0,0 +1,7 @@
+# x = 3
+# print(x)
+
+a = str(3)
+b = int(3)
+c = float(3)
+print(type(a),b,c)
---
+++ python/w3 school /variables/multi.py
@@ -0,0 +1,10 @@
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
