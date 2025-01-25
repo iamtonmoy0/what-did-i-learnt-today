548a7ead1a59626a3f5a8c4562afb2f1014c749e
iamtonmoy0 <tonmoy2code@gmail.com>
2025-01-25 22:02:36 +0600

    Add main.py file to demonstrate various object types in Python

---
+++ python/python revise/data types/main.py
@@ -0,0 +1,14 @@
+# object types in python
+#  Number: 1234,3.45,3+4j,0b11,decimal(),fraction()
+# String : "hello" , 'hello', "bob's"
+# List: [1,2,3], [1,2,3,4,5]
+# Tuple: (1,2,3), (1,2,3,4)
+# Dictionary: {"name":"John","age":30}
+# Set: {1,2,3,4,5}
+# File: open('text.txt')
+# Boolean:True,False
+# None: None
+# Functions,modules,classes
+
+# Advance:Decorators,Generators,Iterators
+
