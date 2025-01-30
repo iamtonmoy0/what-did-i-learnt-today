bc69d9c745aa1152796f9da80680f082f3fd4c99
iamtonmoy0 <tonmoy2code@gmail.com>
2025-01-30 13:53:46 +0600

    Add Hero and Footer components, and update App to include them

--- Next js/LWS/module 2/task-manager/src/App.jsx
+++ Next js/LWS/module 2/task-manager/src/App.jsx
@@ -1,10 +1,14 @@
+import Footer from "./Footer"
 import Header from "./Header"
+import Hero from "./Hero"
 
 function App() {
   return (
     <>
     <Header/>
+    <Hero/>
      welcome to taser
+     <Footer/>
     </>
   )
 }
