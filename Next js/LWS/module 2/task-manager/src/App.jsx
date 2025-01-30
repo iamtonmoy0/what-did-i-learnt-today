65571779756dce10fde791b3e19495b0e86beba5
iamtonmoy0 <tonmoy2code@gmail.com>
2025-01-30 14:31:11 +0600

    Add react-icons dependency and integrate TaskBoard component into App

--- Next js/LWS/module 2/task-manager/src/App.jsx
+++ Next js/LWS/module 2/task-manager/src/App.jsx
@@ -1,13 +1,14 @@
 import Footer from "./Footer"
 import Header from "./Header"
 import Hero from "./Hero"
+import TaskBoard from "./task/TaskBoard"
 
 function App() {
   return (
     <>
     <Header/>
     <Hero/>
-     welcome to taser
+     <TaskBoard/>
      <Footer/>
     </>
   )
