4b56bf2b0265f832f4b739c4f1814f8da95b817a
iamtonmoy0 <tonmoy2code@gmail.com>
2025-03-08 17:58:44 +0600

    Fix PlaceTree component to correctly log child places and render nested lists

--- Next js/LWS/module 3/deep nested state/src/components/PlaceTree.jsx
+++ Next js/LWS/module 3/deep nested state/src/components/PlaceTree.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 export default function PlaceTree(place) {
-    console.log(place)
+    const childPlaces=place.place.childPlaces
+    console.log(childPlaces);
   return (
         <>
       <li>
         {place.place.title}
-        {}
+        {clildPlaces.length>0 && <ol>{childPlaces.map(place=><PlaceTree key={place.id}  />)}</ol>}
 
     </li>
     </>
