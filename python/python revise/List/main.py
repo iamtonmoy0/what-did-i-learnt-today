752015361b687bc42a2fa7c1096a274dee4587bd
iamtonmoy0 <tonmoy2code@gmail.com>
2025-07-14 12:49:54 +0600

    feat: update list examples to demonstrate element replacement and output

--- python/python revise/List/main.py
+++ python/python revise/List/main.py
@@ -4,3 +4,5 @@ hello = ['john','romeo','alpha']
 print(hello[0])  # Output: john
 print(hello[-1])  # Output: alpha
 print(hello[1:3])  # Output: ['romeo', 'alpha']
+hello[1:3] = ['jane', 'doe']  # Replaces 'romeo' and 'alpha' with 'jane' and 'doe'
+print(hello)  # Output: ['john', 'jane', 'doe']
