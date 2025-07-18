a5c4429de24c3bc17cf7cf69725e62853e6a8dc4
iamtonmoy0 <tonmoy2code@gmail.com>
2025-07-19 01:23:16 +0600

    feat: add string examples for length calculation and text search

--- python/python revise/strings/str_fun.py
+++ python/python revise/strings/str_fun.py
@@ -1,1 +1,9 @@
 # different types of string
+a = '''hello world.this is long str'''
+print(a)
+print(len(a))
+
+# text search
+txt = "The rain in Spain stays mainly in the plain."
+print("rain" in txt) #this will return boolean value
+print("hello" not in txt) #this will return boolean value
