5656f04a971dc6118a7ce9d5259c1ee5bc2d8e2f
Tonmoy <tonmoy4671@gmail.com>
2026-06-19 22:58:39 +0600

    kotlin updated

---
+++ Kotlin/Kotlin 101/constructor/src/Main.kt
@@ -0,0 +1,12 @@
+
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+var myCar = Cars()
+    myCar.name="BMW"
+    myCar.model = 5
+    myCar.price=200000
+    println("the car name is ${myCar.name} and the car model is ${myCar.model} and the price is ${myCar.price}")
+}
