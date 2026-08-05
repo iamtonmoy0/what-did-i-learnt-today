854d6d82434e3f722cb0a056b80e598d496f26dd
Tonmoy <tonmoy4671@gmail.com>
2026-08-05 20:16:37 +0600

    dialouge message added

--- Kotlin/Kotlin 101/Android toast and alerts/app/src/main/java/com/example/toastandalert/MainActivity.kt
+++ Kotlin/Kotlin 101/Android toast and alerts/app/src/main/java/com/example/toastandalert/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Button
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var toast: Button
     private lateinit var snack: Button
     private lateinit var layout: ConstraintLayout
+    private lateinit var dialouge: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +25,8 @@ class MainActivity : AppCompatActivity() {
         toast = findViewById(R.id.toast)
         snack=findViewById(R.id.sncakbar)
         layout=findViewById(R.id.main)
+        dialouge=findViewById(R.id.dialougebar)
+
 //        event
         toast.setOnClickListener {
             Toast.makeText(this, "You are gay!", Toast.LENGTH_SHORT).show()
@@ -32,12 +36,30 @@ class MainActivity : AppCompatActivity() {
 
             }.show()
         }
-
+        dialouge.setOnClickListener {
+            showAlertDial()
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun showAlertDial() {
+        AlertDialog.Builder(this)
+            .setTitle("Warning ! a gay person detected")
+            .setMessage("Are you sure you want to exit or die?")
+            .setIcon(R.drawable.warning_24px)
+            .setCancelable(false)
+            .setNegativeButton("no") { dialog, _ ->
+                Toast.makeText(this, " go to hell!", Toast.LENGTH_SHORT).show()
+                dialog.cancel()
+            }
+            .setPositiveButton("yes") { _, _ ->
+                Toast.makeText(this, "Fuck you", Toast.LENGTH_SHORT).show()
+            }
+            .show()
     }
 }
