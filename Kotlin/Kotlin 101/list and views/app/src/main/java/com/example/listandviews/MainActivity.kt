cf5e408a98e3902b648c29c66cc5877e3d61b246
Tonmoy <tonmoy4671@gmail.com>
2026-08-05 20:50:23 +0600

    list view

---
+++ Kotlin/Kotlin 101/list and views/app/src/main/java/com/example/listandviews/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.listandviews
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var listView: ListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        listView = findViewById(R.id.listView)
+        val countryList = resources.getStringArray(R.array.countries)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countryList)
+
+        listView.adapter = adapter
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
